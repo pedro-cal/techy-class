91efdeffbee4cbbdcbad5b734a748cc2bef5e9c3
pedro-cal <pedroluna2@hotmail.com>
2020-10-11 14:11:13 -0300

    Updates

---
+++ precache-manifest.0fe3e533a5f85fc223898d16e0ce3fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1440548a4f700b68c348a734487a071",
+    "url": "/pedro-cal/techy-class.git/index.html"
+  },
+  {
+    "revision": "c66e98a10191c718a016",
+    "url": "/pedro-cal/techy-class.git/static/css/main.6fc3bc25.chunk.css"
+  },
+  {
+    "revision": "30c2456107a7e1ed2ca9",
+    "url": "/pedro-cal/techy-class.git/static/js/2.609799c8.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/pedro-cal/techy-class.git/static/js/2.609799c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66e98a10191c718a016",
+    "url": "/pedro-cal/techy-class.git/static/js/main.ca055036.chunk.js"
+  },
+  {
+    "revision": "40afbf49ad6fefe34d2d",
+    "url": "/pedro-cal/techy-class.git/static/js/runtime-main.84c5ee98.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pedro-cal/techy-class.git/precache-manifest.0fe3e533a5f85fc223898d16e0ce3fa6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pedro-cal/techy-class.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
