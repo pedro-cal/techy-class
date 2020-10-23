638374fc1208742c5e4a8306fe641845ae0e0c49
pedro-cal <pedroluna2@hotmail.com>
2020-10-22 22:15:45 -0300

    Updates

---
+++ precache-manifest.40a8e0553953cbfc1fd6ec5d03007127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a14a930bda056d8dbb2267b2e90eaa",
+    "url": "/techy-class/index.html"
+  },
+  {
+    "revision": "36b66a34930c80b951fb",
+    "url": "/techy-class/static/css/main.8257c9c2.chunk.css"
+  },
+  {
+    "revision": "219fbb5cc93b21c9555c",
+    "url": "/techy-class/static/js/2.adea5ce3.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/techy-class/static/js/2.adea5ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b66a34930c80b951fb",
+    "url": "/techy-class/static/js/main.baa742f3.chunk.js"
+  },
+  {
+    "revision": "9e1b7f4ab10b0473de70",
+    "url": "/techy-class/static/js/runtime-main.4081bf29.js"
+  }
+]);
